a353ddfe1e537f8150bb9aeb1e23cfc68e4f8ffe
Carrie <carolyn.anne.clark@gmail.com>
2019-02-09 11:19:28 -0600

    Implemented: press key to start game, random button blinks/sound plays

---
+++ course-simon-game/game.js
@@ -0,0 +1,53 @@
+let gameStarted = false; //Making sure the keydown event only starts the game once
+
+$(document).keydown(function () {
+  if (!gameStarted) {
+    gameStarted = true;
+
+    const buttonColors = ["red", "blue", "green", "yellow"];
+    let randomChosenColor = buttonColors[nextSequence()];
+
+    let gamePattern = [];
+    gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeOut(125).fadeIn(125); //Make the button of the chosen color blink
+    
+    playSound(randomChosenColor);
+
+    console.log(gamePattern);
+    console.log(randomChosenColor);
+  }
+});
+
+function nextSequence() {
+  return Math.floor(Math.random() * 4);
+}
+
+function playSound(colorName) {
+  switch (colorName) {
+    case "blue":
+      let soundBlue = new Audio("sounds/blue.mp3");
+      soundBlue.play();
+      break;
+
+    case "green":
+      let soundGreen = new Audio("sounds/green.mp3");
+      soundGreen.play();
+      break;
+
+    case "red":
+      let soundRed = new Audio("sounds/red.mp3");
+      soundRed.play();
+      break;
+
+    case "yellow":
+      let soundYellow = new Audio("sounds/yellow.mp3");
+      soundYellow.play();
+      break;
+
+    case "wrong":
+      let soundWrong = new Audio("sounds/wrong.mp3");
+      soundWrong.play();
+      break;
+  }
+}
