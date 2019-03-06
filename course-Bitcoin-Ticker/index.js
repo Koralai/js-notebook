29c2fdf9eb40f6308159abbc621b96bdfe918ebb
Carrie <carolyn.anne.clark@gmail.com>
2019-03-06 16:31:19 -0600

    Add functionality for converting the chosen cryptocurrency from an abbreviation to a word

--- course-Bitcoin-Ticker/index.js
+++ course-Bitcoin-Ticker/index.js
@@ -14,16 +14,32 @@ app.post("/", function(req, res){
   let userChosenCrypto = req.body.crypto;
   let userChosenFiat = req.body.fiat;
 
+  let cryptoWord = getCryptoWord(userChosenCrypto);
+
   let tickerRequestURL = `https://apiv2.bitcoinaverage.com/indices/global/ticker/${userChosenCrypto}${userChosenFiat}`; 
 
   request(tickerRequestURL, function(error, response, body) {
     let data = JSON.parse(body); // body is the JSON data sent in response; convert to JS object to work with it
     let latestPrice = data.last; // "last" points to a key/value pair from the API data
 
-    res.send(`The current price of ${userChosenCrypto} is ${latestPrice} ${userChosenFiat}.`);
+    res.send(`The current price of ${cryptoWord} is ${latestPrice} ${userChosenFiat}.`);
   });
 });
 
 app.listen(3000, function() {
   console.log("Server is running on port 3000.")
 });
+
+// Convert the user's chosen cryptocurrency to a word to make the response more readable:
+function getCryptoWord(cryptoAbbreviation) {
+  switch(cryptoAbbreviation) {
+    case "BTC":
+      return "Bitcoin"; 
+    case "ETH":
+      return "Ethereum";
+    case "LTC":
+      return "Litecoin";
+    default: 
+      return "this cryptocurrency";
+  }
+}
