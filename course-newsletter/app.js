36be85824b5009f54bd059125f3c4e69e165fb9a
Carrie <carolyn.anne.clark@gmail.com>
2019-03-10 12:38:10 -0500

    Add basic POST method

--- course-newsletter/app.js
+++ course-newsletter/app.js
@@ -5,10 +5,19 @@ const request = require("request");
 const bodyParser = require("body-parser");
 
 app.use(express.static(`public`));
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
   res.sendFile(`${__dirname}/signup.html`);
 });
+
+app.post("/", function(req, res){
+  res.send(`
+  First name: ${req.body.firstName}
+  Last name: ${req.body.lastName}
+  Email: ${req.body.email}
+  `);
+})
 
 app.listen(3000, function(){
   console.log(`Server is running on port 3000.`)
