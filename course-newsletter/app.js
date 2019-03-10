fdd7abf794b9120f5b239f042f1b28b70b37d6b4
Carrie <carolyn.anne.clark@gmail.com>
2019-03-10 12:25:15 -0500

    Add styling and static files for the home route

--- course-newsletter/app.js
+++ course-newsletter/app.js
@@ -4,6 +4,12 @@ const app = express();
 const request = require("request");
 const bodyParser = require("body-parser");
 
+app.use(express.static(`public`));
+
+app.get("/", function(req, res){
+  res.sendFile(`${__dirname}/signup.html`);
+});
+
 app.listen(3000, function(){
   console.log(`Server is running on port 3000.`)
 });
